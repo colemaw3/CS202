06355b0ecfddb08b72d0d8224463e8eedaa2e51c
Wyatt Coleman <colemaw3@wwu.edu>
2019-02-15 13:58:16 -0800

    JavaScript lesson

---
+++ js/script.js
@@ -0,0 +1,87 @@
+/* window .alert("hello world"); */
+
+//statments
+
+/* window.alert("69");
+ */
+
+ /* Printing out informtion */
+/*  window.alert('swag money brotheren');
+ */
+
+/* document.write('what is your email?');
+
+prompt('Enter your name!');
+confirm('do you agree?'); */
+/* console.log('In console now');
+ */
+
+// parseInt(prompt('Enter your age.'));
+//Element.innerHtml;
+//varibles.
+var Gspot = 24;
+var text = "Wyatt is the man:)";
+/* alert(text);
+document.write(Gspot + ' ' + text); */
+/* document.write('<h2> Hello, ' + text + '</h2>');
+
+var date = Date();
+document.write(date);
+
+var userColor = prompt('Enter your favorite color?')
+
+document.bgColor = userColor; */
+
+//////////////////  Data Types: numbers,strings,objects,arrays
+
+/* var num = 2333;
+var txt = "Wyatt";
+var gameOver = true; // or false
+
+var student = (
+    Name:"Wyatt",
+    Age:"20";
+    eyeColor:"green";
+)
+var colors = ['green','red','black'];
+
+document.write(colors[0]) */
+
+////////////// Functions: blocks of codes used for specific purpose
+
+/* function functionname(){
+    //run your function code
+    alert('Hi New User');
+}
+// call the function
+functionname();
+ */
+/* function doSomething(){
+    /* document.getElementById('java').innerHTML = 'New JavaScript For Class.'; */
+    //var x = document.getElementById('java');
+    //x.style.backgroundColor = 'red';
+    //DOM: document object model
+/* 
+    document.querySelector('java').style.fontSize = '1em';
+} */
+
+
+//////////////// Events: actions or something that the user reacts to.
+
+//click, load, resize, mouseover, blur, change, mouseout, keydown, mouse 
+
+
+//////////////////EventListener
+var x = document.querySelector('.btn');
+
+x.addEventListener('click', function(){
+    document.getElementById('java').style.color = 'pink';
+})
+
+
+
+
+
+
+
+
